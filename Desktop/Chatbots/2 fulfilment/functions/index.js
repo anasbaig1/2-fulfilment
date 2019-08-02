7d7442ee16e9afa01210f6fef4fdb2ffdc773e48
anasbaig <anasmasood_2002@hotmail.com>
2019-08-02 21:50:44 +0500

    1st commit

---
+++ Desktop/Chatbots/2 fulfilment/functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.webhook = functions.https.onRequest((request, response) => {
+ console.log("request.body.result.parameters: ",request.body.result.parameters);
+ let params = req.body.result.parameters;
+ response.send({
+     fulfillmentText:
+        '${params.name} your hotel booking request for ${params.rtype} room is forwarded for ${params.persons} persons, we will contact you on ${params.email} soon'
+ });
+});
